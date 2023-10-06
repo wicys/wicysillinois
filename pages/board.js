42df5afa4c8ebcbcadaa2e48368172987ef5cde5
anshimathur0325 <anshimathur0325@gmail.com>
2023-10-06 16:17:35 -0500

    Update the board page and add photos

--- pages/board.js
+++ pages/board.js
@@ -4,19 +4,44 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div >
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Board Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title="Executive Board Members" />
+        <div className='boardimages'>
+        <p className="description"> 
+          wicys@illinois:~$ cd ..
+          <br/>wicys@illinois:~$ 
+          <br/>cat Board.txt
         </p>
+        <br></br>
+        <h3>Meet the 2023-2023 Board!</h3>
+        <p>wicys@illinois:~$ su President<br/>President@illinois:~$ whoami</p>
+        <p>Sophia Liu<br/>President<br/>Sophomore in Computer Science</p>
+        <img src = "/prez.jpeg"></img>
+        <p>wicys@illinois:~$ su ExternalVicePresident<br/>ExternalVicePresident@illinois:~$ whoami</p>
+        <p>Priya Dutta<br/>External Vice President<br/>Junior in Computer Engineering</p>
+        <img src = "/extvp.png"></img>
+        <p>wicys@illinois:~$ su InternalVicePresident<br/>InternalVicePresident@illinois:~$ whoami</p>
+        <p>Abigail Obradovic<br/>Internal Vice President<br/>Sophomore in Computer Science</p>
+        <img src = "/intvp.png"></img>
+        <p>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>  
+        <p>Nicole Gromski<br/>Technical Lead<br/>Junior in Information Science</p>
+        <img src = "/nicoletechlead.png"></img>
+        <p>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>
+        <p>Athena Fung<br/>Technical Lead<br/>Junior in Computer Science</p>
+        <img src = "/athenatechlead.jpeg"></img>
+        <p>wicys@illinois:~$ su Treasurer<br/>Treasurer@illinois:~$ whoami</p>
+        <p>Sena Tiryaki<br/>Treasurer<br/>Freshman in Computer Engineering</p>
+        <p>wicys@illinois:~$ su PublicRelationsChair<br/>PublicRelationsChair@illinois:~$ whoami</p>
+        <p>Alejandra Arias<br/>Public Relations Chair<br/>Sophomore in Information Science + Computer Science Minor</p>
+        <img src = "/pubrelations.png"></img>
+        </div>
       </main>
-
       <Footer />
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <a href = '/board'>board</a>
       </main>
 
       <Footer />
