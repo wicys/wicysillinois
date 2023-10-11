b789d797ad242415042325050b4502c3ccb55b23
Sujin Shin <sujins2@illinois.edu>
2023-10-11 02:45:13 -0500

    added dependencies for icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default Application
+export default Application;
