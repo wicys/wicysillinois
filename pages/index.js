d54775b16d005cc4049a84c112b76e7d61c384c7
Karena Liang <ksliang2@illinois.edu>
2023-10-11 21:19:20 -0500

    added header and description

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,26 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <div className="homepage">
+      <h1>WELCOME TO WOMEN IN CYBERSECURITY</h1>
+      <p> wicys@illinois: ~$ cd WiCyS/About_us </p>
+      <p> wicys@illinois: ~$ cat about.txt </p>
+      
+      
+      <p>
+        Women in Cybersecurity Illinois is the UIUC student chapter of the national organization dedicated to bringing together women in cybersecurity from academia, research, and industry to share knowledge, experience, mentoring, and networking.
+      </p>
+      <p>
+        Our community is for those interested in learning more about the growing field of cybersecurity through tech talks, workshops, netowrking opportunities, research, conferences, and leadership opportunities. 
+      </p>
+      <p>
+        We have general meetings on [Day of the Week] at [Time CST] every 2 weeks. Can’t make it? We have a bi-weekly newsletter!
+      </p>
+      
+      <p> wicys@illinois: ~$ cd ..  </p>
+      <p> wicys@illinois: ~$ cd Home  </p>
+      <p> wicys@illinois: ~$ cat homepage.txt  </p>
+      
     </div>
   )
 }
