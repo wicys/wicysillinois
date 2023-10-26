2f327f72d67804a6e5fc185e03cfb5eda8fec76a
Porscha <phend4@illinois.edu>
2023-10-26 02:49:44 -0500

    added basic outline for text, divided by semester

--- pages/events.js
+++ pages/events.js
@@ -9,12 +9,49 @@ export default function Events() {
         <title>WiCyS Illinois</title>
         <link rel="icon" href="https://www.wicys.org/wp-content/uploads/2020/10/favicon-wicys.png" />
       </Head>
+      <Header />
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1> EVENTS </h1>
+        <p>Overview of meetings and when they occur</p>
+        <div id="FA2023">
+          <h4>Fall 2023</h4>
+          <ul style="list-style-type: none;">
+            <li>Meeting 1</li>
+            <li>Meeting 2</li>
+            <li>Meeting 3</li>
+            <li>Meeting 4</li>
+            <li>Meeting 5</li>
+          </ul>
+        </div>
+
+        <div id="SPR23">
+          <h4>Spring 2023</h4>
+          <ul style="list-style-type: none;">
+            <li>Meeting 1</li>
+            <li>Meeting 2</li>
+            <li>Meeting 3</li>
+            <li>Meeting 4</li>
+            <li>Meeting 5</li>
+            <li>Meeting 6</li>
+            <li>Meeting 7</li>
+            <li>Meeting 8</li>
+          </ul>
+        </div>
+
+        <div id="FA22">
+          <h4>Fall 2022</h4>
+          <ul style="list-style-type: none;">
+            <li>Meeting 1</li>
+            <li>Meeting 2</li>
+            <li>Meeting 3</li>
+            <li>Meeting 4</li>
+            <li>Meeting 5</li>
+            <li>Meeting 6</li>
+            <li>Meeting 7</li>
+            <li>Meeting 8</li>
+          </ul>
+        </div>
       </main>
 
       <Footer />
