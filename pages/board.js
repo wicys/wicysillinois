7233d5195e761c62d57765a2f4493fd423902192
Karena Liang <ksliang2@illinois.edu>
2023-10-31 11:58:16 -0500

    additions to main page coloring

--- pages/board.js
+++ pages/board.js
@@ -13,45 +13,45 @@ export default function Board() {
         <Header title="Executive Board Members" />
         <div className='boardimages'>
         <p className="description"> 
-          wicys@illinois:~$ cd ..
-          <br/>wicys@illinois:~$ 
-          <br/>cat Board.txt
+          <span className='wicys-green'>wicys@illinois</span>:~$ cd ..
+          <br/><span className='wicys-green'>wicys@illinois</span>:~$ cd Board
+          <br/><span className='wicys-green'>wicys@illinois</span>:~$ cat Board.txt
         </p>
         <br></br>
         <h1>Meet the 2023-2023 Board!</h1>
-        <p>wicys@illinois:~$ su President<br/>President@illinois:~$ whoami</p>
+        <p><span className='wicys-green'>wicys@illinois</span>:~$ su President<br/><span className='wicys-green'>President@illinois</span>:~$ whoami</p>
         <p class='name'>Sophia Liu<br/>President<br/>Sophomore in Computer Science</p>
         <img src = "/prez.jpg"></img>
 
-        <p>wicys@illinois:~$ su ExternalVicePresident<br/>ExternalVicePresident@illinois:~$ whoami</p>
+        <p><span className='wicys-green'>wicys@illinois</span>:~$ su ExternalVicePresident<br/><span className='wicys-green'>ExternalVicePresident@illinois</span>:~$ whoami</p>
         <p class='name'>Priya Dutta<br/>External Vice President<br/>Junior in Computer Engineering</p>
         <img src = "/extvp.png"></img>
 
-        <p>wicys@illinois:~$ su InternalVicePresident<br/>InternalVicePresident@illinois:~$ whoami</p>
+        <p><span className='wicys-green'>wicys@illinois</span>:~$ su InternalVicePresident<br/><span className='wicys-green'>InternalVicePresident@illinois</span>:~$ whoami</p>
         <p class='name'>Abigail Obradovic<br/>Internal Vice President<br/>Sophomore in Computer Science</p>
         <img src = "/intvp.png"></img>
 
-        <p>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>  
+        <p><span className='wicys-green'>wicys@illinois</span>:~$ su TechnicalLead<br/><span className='wicys-green'>TechnicalLead@illinois</span>:~$ whoami</p>  
         <p class='name'>Nicole Gromski<br/>Technical Lead<br/>Junior in Information Science</p>
         <img src = "/nicoletechlead.png"></img>
 
-        <p>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>
+        <p><span className='wicys-green'>wicys@illinois</span>:~$ su TechnicalLead<br/><span className='wicys-green'>TechnicalLead@illinois</span>:~$ whoami</p>
         <p class='name'>Athena Fung<br/>Technical Lead<br/>Junior in Computer Science</p>
         <img src = "/athenatechlead.png"></img>
 
-        <p>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>
+        <p><span className='wicys-green'>wicys@illinois</span>:~$ su TechnicalLead<br/><span className='wicys-green'>TechnicalLead@illinois</span>:~$ whoami</p>
         <p class='name'>Japjeev Kaur<br/>Technical Lead<br/>Freshman in Computer Science and Economics</p>
         <img src = "/japjeevtechlead.png"></img>
 
-        <p>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>
+        <p><span className='wicys-green'>wicys@illinois</span>:~$ su TechnicalLead<br/><span className='wicys-green'>TechnicalLead@illinois</span>:~$ whoami</p>
         <p class='name'>Kushi<br/>Technical Lead<br/>Sophomore in Computer Engineering</p>
         <img src = "/kushitechlead.png"></img>
 
-        <p>wicys@illinois:~$ su Treasurer<br/>Treasurer@illinois:~$ whoami</p>
+        <p><span className='wicys-green'>wicys@illinois</span>:~$ su Treasurer<br/><span className='wicys-green'>Treasurer@illinois</span>:~$ whoami</p>
         <p class='name'>Sena Tiryaki<br/>Treasurer<br/>Freshman in Computer Engineering</p>
         <img src = "/treasurer.png"></img>
 
-        <p>wicys@illinois:~$ su PublicRelationsChair<br/>PublicRelationsChair@illinois:~$ whoami</p>
+        <p><span className='wicys-green'>wicys@illinois</span>:~$ su PublicRelationsChair<br/><span className='wicys-green'>PublicRelationsChair@illinois</span>:~$ whoami</p>
         <p class='name'>Alejandra Arias<br/>Public Relations Chair<br/>Sophomore in Information Science + Computer Science Minor</p>
         <img src = "/pubrelations.png"></img>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -16,37 +16,40 @@ export default function Home() {
       <Header />
       <main>
         
-          <h1>WELCOME TO WOMEN IN CYBERSECURITY</h1>
-          <p> wicys@illinois: ~$ cd WiCyS/About_us </p>
-          <p> wicys@illinois: ~$ cat about.txt </p>
+          <h1> <span className='wicys-purple'>WELCOME TO WOMEN IN CYBERSECURITY</span></h1>
+          <p> <span className='wicys-green'>wicys@illinois</span>: ~$ cd WiCyS/About_us </p>
+          <p> <span className='wicys-green'>wicys@illinois</span>: ~$ cat about.txt </p>
 
 
           <p>
-            Women in Cybersecurity Illinois is the UIUC student chapter of the national organization dedicated to bringing together women in cybersecurity from academia, research, and industry to share knowledge, experience, mentoring, and networking.
+          <span className='wicys-purple'>Women</span> in <span className='wicys-green'>Cybersecurity</span> <span className='wicys-blue'>Illinois</span> is the UIUC student chapter of the national organization dedicated to bringing together women in cybersecurity from academia, research, and industry to share knowledge, experience, mentoring, and networking.
           </p>
           <p>
             Our community is for those interested in learning more about the growing field of cybersecurity through tech talks, workshops, netowrking opportunities, research, conferences, and leadership opportunities.
           </p>
           <p>
-            We have general meetings on Mondays from 6 to 7 PM every 2 weeks. Can’t make it? We have a bi-weekly newsletter!
+            We have general meetings on Mondays from 6 to 7 PM every 2 weeks. Can’t make it? We have a bi-weekly <span className='wicys-purple'>newsletter</span>!
           </p>
 
-          <p> wicys@illinois: ~$ cd ..  </p>
-          <p> wicys@illinois: ~$ cd Home  </p>
-          <p> wicys@illinois: ~$ cat homepage.txt  </p>
+          <p> <span className='wicys-green'>wicys@illinois</span>: <span className='wicys-purple'>~</span><span className='wicys-blue'>$</span> cd ..  </p>
+          <p> <span className='wicys-green'>wicys@illinois</span>: <span className='wicys-purple'>~</span><span className='wicys-blue'>$</span> cd Home  </p>
+          <p> <span className='wicys-green'>wicys@illinois</span>: <span className='wicys-purple'>~</span><span className='wicys-blue'>$</span> cat homepage.txt  </p>
 
         
-        
-          <h2>Events</h2>
+
+            <img src="photo1.png" alt="picture1"/>
+
+
+          <h2><span className='wicys-purple'>Events</span></h2>
           <p>
             We host general meetings every 2 weeks ranging from cryptography to command line workshops. Be sure to come network with company partners and learn with peers!
-            Check out our calendar to never miss our meetings!
+            Check out our <span className='wicys-blue'>calendar</span> to never miss our meetings!
           </p>
-          <h2>EOH Game</h2>
-          <p>Every year we recruit individuals who are interested in gaining programming experience and incorporating cybersecurity topics into a security themed game! Be sure to check out our past games and if interested to contribute, check out our Discord channel for meetings!
+          <h2><span className='wicys-purple'>EOH Game</span></h2>
+          <p>Every year we recruit individuals who are interested in gaining programming experience and incorporating cybersecurity topics into a security themed game! Be sure to check out our past games and if interested to contribute, check out our <span className='wicys-green'>Discord</span> channel for meetings!
           </p>
-          <h3>READY TO JOIN?</h3>
-          <p>Women in Cybersecurity Illinois is free to join and open to anyone! Don’t be discouraged if you do not identify as a woman or have little to no cybersecurity knowledge. The best way to get involved is to join our Discord and follow our Instagram! There, you will find reminders for meetings and past events we host, feel free to ask any questions you may have. </p>
+          <h3><span className='wicys-purple'>READY TO JOIN?</span></h3>
+          <p><span className='wicys-purple'>Women</span> in <span className='wicys-green'>Cybersecurity</span> <span className='wicys-blue'>Illinois</span> is free to join and open to anyone! Don’t be discouraged if you do not identify as a woman or have little to no cybersecurity knowledge. The best way to get involved is to join our <span className='wicys-green'>Discord</span> and follow our <span className='wicys-blue'>Instagram</span>! There, you will find reminders for meetings and past events we host, feel free to ask any questions you may have. </p>
           <p>We also host social events such as pumpkin painting and study sessions when you want to relax! Join our newsletter to receive bi-weekly updates so you never miss out from us. </p>
         
       </main>
