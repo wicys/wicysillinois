bd7c634d206b981bec9f8721595f61ddbff899e2
Sophia Liu <sophialiu7436@gmail.com>
2025-02-14 12:41:24 -0600

    Update board members page

--- pages/board.js
+++ pages/board.js
@@ -19,66 +19,66 @@ export default function Board() {
         </p>
         <br></br>
         <hr></hr>
-        <img src = "/prez.jpg"></img>
+        <img src = "/abbyobradovic.png"></img>
         <p class={styles.description}>wicys@illinois:~$ su President<br/>President@illinois:~$ whoami</p>
-        <p class={styles.boardnames}>Sophia Liu<br/>President<br/>Sophomore in Computer Science</p>
+        <p class={styles.boardnames}>Abby Obradovic<br/>President<br/>Junior in Computer Science</p>
         <br/>
         <br/>
         <br/>
         <hr></hr>
         <div/>
-        <img src = "/extvp.png"></img>
-        <p class={styles.description}>wicys@illinois:~$ su ExternalVicePresident<br/>ExternalVicePresident@illinois:~$ whoami</p>
-        <p class={styles.boardnames}>Priya Dutta<br/>External Vice President<br/>Junior in Computer Engineering</p>
+        <img src = "/japjeevkaur.png"></img>
+        <p class={styles.description}>wicys@illinois:~$ su VicePresident<br/>VicePresident@illinois:~$ whoami</p>
+        <p class={styles.boardnames}>Japjeev Kaur<br/>Vice President<br/>Sophomore in Computer Science + Econ</p>
         <br/>
         <br/>
         <br/>
         <hr></hr>
-        <img src = "/intvp.png"></img>
-        <p class={styles.description}>wicys@illinois:~$ su InternalVicePresident<br/>InternalVicePresident@illinois:~$ whoami</p>
-        <p class={styles.boardnames}>Abigail Obradovic<br/>Internal Vice President<br/>Sophomore in Computer Science</p>
+        <img src = "/sophialiu.png"></img>
+        <p class={styles.description}>wicys@illinois:~$ su StudentAdvisor<br/>StudentAdvisor@illinois:~$ whoami</p>
+        <p class={styles.boardnames}>Sophia Liu<br/>Student Advisor<br/>Junior in Computer Science</p>
         <br/>
         <br/>
         <br/>
         <hr></hr>
-        <img src = "/nicoletechlead.png"></img>
-        <p class={styles.description}>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>  
-        <p class={styles.boardnames}>Nicole Gromski<br/>Technical Lead<br/>Junior in Information Science</p>
+        <img src = "/alejandraarias.png"></img>
+        <p class={styles.description}>wicys@illinois:~$ su CorporateChair<br/>CorporateChair@illinois:~$ whoami</p>  
+        <p class={styles.boardnames}>Alejandra Arias<br/>Corporate Chair<br/>Junior in Information Sciences + CS Minor</p>
         <br/>
         <br/>
         <br/>
         <hr></hr>
-        <img src = "/athenatechlead.png"></img>
-        <p class={styles.description}>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>
-        <p class={styles.boardnames}>Athena Fung<br/>Technical Lead<br/>Junior in Computer Science</p>
-        <br/>
-        <br/>
-        <br/>
-        <hr></hr>
-        <img src = "/japjeevtechlead.png"></img>
-        <p class={styles.description}>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>
-        <p class={styles.boardnames}>Japjeev Kaur<br/>Technical Lead<br/>Freshman in Computer Science and Economics</p>
-        <br/>
-        <br/>
-        <br/>
-        <hr></hr>
-        <img src = "/kushitechlead.png"></img>
-        <p class={styles.description}>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>
-        <p class={styles.boardnames}>Kushi Kalra<br/>Technical Lead<br/>Sophomore in Computer Engineering</p>
-        <br/>
-        <br/>
-        <br/>
-        <hr></hr>
-        <img src = "/treasurer.png"></img>
-        <p class={styles.description}>wicys@illinois:~$ su Treasurer<br/>Treasurer@illinois:~$ whoami</p>
-        <p class={styles.boardnames}>Sena Tiryaki<br/>Treasurer<br/>Freshman in Computer Engineering</p>
-        <br/>
-        <br/>
-        <br/>
-        <hr></hr>
-        <img src = "/pubrelations.png"></img>
+        <img src = "/elinalee.png"></img>
         <p class={styles.description}>wicys@illinois:~$ su PublicRelationsChair<br/>PublicRelationsChair@illinois:~$ whoami</p>
-        <p class={styles.boardnames}>Alejandra Arias<br/>Public Relations Chair<br/>Sophomore in Information Science + Computer Science Minor</p>
+        <p class={styles.boardnames}>Elina Lee<br/>Public Relations Chair<br/>Freshman in Information Sciences</p>
+        <br/>
+        <br/>
+        <br/>
+        <hr></hr>
+        <img src = "/allystedman.png"></img>
+        <p class={styles.description}>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>
+        <p class={styles.boardnames}>Ally Stedman<br/>Technical Lead<br/>Freshman in Computer Science</p>
+        <br/>
+        <br/>
+        <br/>
+        <hr></hr>
+        <img src = "/alyssaflores.png"></img>
+        <p class={styles.description}>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>
+        <p class={styles.boardnames}>Alyssa Flores<br/>Technical Lead<br/>Junior in Computer Science</p>
+        <br/>
+        <br/>
+        <br/>
+        <hr></hr>
+        <img src = "/kashvipanjolia.png"></img>
+        <p class={styles.description}>wicys@illinois:~$ su TechnicalLead<br/>Treasurer@illinois:~$ whoami</p>
+        <p class={styles.boardnames}>Kashvi Panjolia<br/>Technical Lead<br/>Sophomore in Computer Science</p>
+        <br/>
+        <br/>
+        <br/>
+        <hr></hr>
+        <img src = "/madisonlee.png"></img>
+        <p class={styles.description}>wicys@illinois:~$ su TechnicalLead<br/>TechnicalLead@illinois:~$ whoami</p>
+        <p class={styles.boardnames}>Madison Lee<br/>Technical Lead<br/>Freshman in Information Sciences</p>
         </div>
       </main>
       <Footer />
