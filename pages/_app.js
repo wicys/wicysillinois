e7f52fe4db6ab051bda3da0a999991f24658ce55
japjeevkaur <144073245+japjeevkaur@users.noreply.github.com>
2025-06-21 23:22:19 -0500

    Updated the events page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import "@styles/globals.css";
+
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
+
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- pages/events.js
+++ pages/events.js
@@ -1,23 +1,61 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import styles from "../styles/events.module.css"; 
 
-export default function Events() {
+export default function events() {
+  const events = [
+    {
+      title: "Technical Workshops",
+      image: "/technical.jpg",
+      description:
+        "Hone your technical skills with hands-on coding sessions, CTFs, reverse engineering, and cybersecurity tool demos.",
+    },
+    {
+      title: "Social Events",
+      image: "/social.jpg",
+      description:
+        "Build lasting friendships through game nights, ice cream socials, movie screenings, and bonding activities.",
+    },
+    {
+      title: "Corporate Events",
+      image: "professional.jpg",
+      description:
+        "Get career-ready with resume workshops, mock interviews, and networking nights hosted by top companies in cybersecurity.",
+    },
+  ];
+
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
-        <title>WiCyS Illinois</title>
-        <link rel="icon" href="https://www.wicys.org/wp-content/uploads/2020/10/favicon-wicys.png" />
+        <title>WiCyS Illinois | Events</title>
+        <link
+          rel="icon"
+          href="https://www.wicys.org/wp-content/uploads/2020/10/favicon-wicys.png"
+        />
       </Head>
-      <Header/>
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+
+      <Header />
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Our Events</h1>
+
+        <div className={styles.eventRow}>
+          {events.map((event, index) => (
+            <div key={index} className={styles.card}>
+              <img
+                src={event.image}
+                alt={event.title}
+                className={styles.image}
+              />
+              <h2 className={styles.cardTitle}>{event.title}</h2>
+              <p className={styles.cardDescription}>{event.description}</p>
+            </div>
+          ))}
+        </div>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
